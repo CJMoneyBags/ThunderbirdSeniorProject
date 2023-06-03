95d8992c62104455f684208955ffbc1c1dea2c14
GrantWilliams99 <grantwilliams99@byui.edu>
2023-06-03 16:33:00 -0600

    Added getAll, getAllNames, delete, and documentation

--- app/src/main/java/com/seniorproj/thunderbird/Database.kt
+++ app/src/main/java/com/seniorproj/thunderbird/Database.kt
@@ -1,12 +1,22 @@
 package com.seniorproj.thunderbird
 
+import android.util.Log
 import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.tasks.await
 
+/**********************************************************************************************
+ * Contains data classes and methods for using the Database
+ *********************************************************************************************/
 object Database {
+    private const val TAG: String = "DATABASE"
+
+    /**********************************************************************************************
+     * Represents a piece of Cargo
+     *********************************************************************************************/
     data class Cargo(
         val name: String = "",
         val contents: String = "",
@@ -16,6 +26,9 @@ object Database {
         val weight: Double = 0.0
     )
 
+    /**********************************************************************************************
+     * Represents a Container
+     *********************************************************************************************/
     data class Container(
         val name: String = "",
         val length: Double = 0.0,
@@ -24,26 +37,105 @@ object Database {
         val weightLimit: Double = 0.0
     )
 
+    /**********************************************************************************************
+     * Gets a single Cargo document from the database.
+     * @param String document name
+     * @return [Database.Cargo] or null
+     *********************************************************************************************/
     suspend fun getCargo(document: String): Cargo? {
-        val data = getData("cargo", document)
+        val data = getDocument("cargo", document)
         return data?.toObject<Cargo>()
     }
 
-    suspend fun setCargo(data: Cargo, name: String = data.name): Boolean {
-        return setData("cargo", name, data)
+    /**********************************************************************************************
+     * Gets all Cargo documents from the database.
+     * @return [List]<[Database.Cargo]>
+     *********************************************************************************************/
+    suspend fun getAllCargo(): List<Cargo> {
+        val data = getAllDocuments("cargo")
+        return data?.documents?.mapNotNull { it.toObject<Cargo>() } ?: listOf()
     }
 
+    /**********************************************************************************************
+     * Gets all Cargo documents' names from the database.
+     * @return [List]<[String]>
+     *********************************************************************************************/
+    suspend fun getAllCargoNames(): List<String> {
+        return getAllCargo().map { it.name }
+    }
+
+    /**********************************************************************************************
+     * Puts a new Cargo document into the database.
+     * @param Database.Cargo data class
+     * @param String document name ([Database.Cargo.name] by default)
+     * @return [Boolean] true if successful, false if not
+     *********************************************************************************************/
+    suspend fun setCargo(data: Cargo, document: String = data.name): Boolean {
+        return setDocument("cargo", document, data)
+    }
+
+    /**********************************************************************************************
+     * Deletes a Cargo document from the database.
+     * @param String document name
+     * @return [Boolean] true if successful, false if not
+     *********************************************************************************************/
+    suspend fun deleteCargo(document: String): Boolean {
+        return deleteDocument("cargo", document)
+    }
+
+    /**********************************************************************************************
+     * Gets a single Container document from the database.
+     * @param String document name
+     * @return [Database.Container] or null
+     *********************************************************************************************/
     suspend fun getContainer(document: String): Container? {
-        val data = getData("containers", document)
+        val data = getDocument("containers", document)
         return data?.toObject<Container>()
     }
 
-    suspend fun setContainer(data: Container, name: String = data.name): Boolean {
-        return setData("containers", name, data)
+    /**********************************************************************************************
+     * Gets all Container documents from the database.
+     * @return [List]<[Database.Container]>
+     *********************************************************************************************/
+    suspend fun getAllContainers(): List<Container> {
+        val data = getAllDocuments("containers")
+        return data?.documents?.mapNotNull { it.toObject<Container>() } ?: listOf()
     }
 
-    // shared functions
-    private suspend fun getData(collection: String, document: String): DocumentSnapshot? {
+    /**********************************************************************************************
+     * Gets all Container documents' names from the database.
+     * @return [List]<[String]>
+     *********************************************************************************************/
+    suspend fun getAllContainerNames(): List<String> {
+        return getAllContainers().map { it.name }
+    }
+
+    /**********************************************************************************************
+     * Puts a new Container document into the database.
+     * @param Database.Container data class
+     * @param String document name ([Database.Container.name] by default)
+     * @return [Boolean] true if successful, false if not
+     *********************************************************************************************/
+    suspend fun setContainer(data: Container, document: String = data.name): Boolean {
+        return setDocument("containers", document, data)
+    }
+
+    /**********************************************************************************************
+     * Deletes a Container document from the database.
+     * @param String document name
+     * @return [Boolean] true if successful, false if not
+     *********************************************************************************************/
+    suspend fun deleteContainer(document: String): Boolean {
+        return deleteDocument("containers", document)
+    }
+
+    /**********************************************************************************************
+     * Gets a document from a collection the database.
+     * @param String collection name
+     * @param String document name
+     * @return [DocumentSnapshot]?
+     *********************************************************************************************/
+    private suspend fun getDocument(collection: String, document: String): DocumentSnapshot? {
         return try {
             val data = Firebase.firestore
                 .collection(collection)
@@ -53,11 +145,38 @@ object Database {
             data
         }
         catch (e: Exception) {
+            Log.d(TAG, "Unable to get $document from $collection due to $e")
             null
         }
     }
 
-    private suspend fun setData(collection: String, document: String, data: Any): Boolean {
+    /**********************************************************************************************
+     * Gets all documents from a collection in the database.
+     * @param String collection name
+     * @return [QuerySnapshot]?
+     *********************************************************************************************/
+    private suspend fun getAllDocuments(collection: String): QuerySnapshot? {
+        return try {
+            val data = Firebase.firestore
+                .collection(collection)
+                .get()
+                .await()
+            data
+        }
+        catch (e: Exception) {
+            Log.d(TAG, "Unable to get all documents from $collection due to $e")
+            null
+        }
+    }
+
+    /**********************************************************************************************
+     * Puts a new document into a collection in the database.
+     * @param String collection name
+     * @param String document name
+     * @param Any Cargo or Container data class
+     * @return [Boolean] true if successful, false if not
+     *********************************************************************************************/
+    private suspend fun setDocument(collection: String, document: String, data: Any): Boolean {
         // make sure we have a Cargo or Container object
         if (!(data is Container || data is Cargo)) {
             return false
@@ -73,6 +192,28 @@ object Database {
             true
         }
         catch (e: Exception) {
+            Log.d(TAG, "Unable to get set new data into $document in $collection due to $e")
+            false
+        }
+    }
+
+    /**********************************************************************************************
+     * Deletes a document from a collection in the database.
+     * @param String collection name
+     * @param String document name
+     * @return [Boolean] true if successful, false if not
+     *********************************************************************************************/
+    private suspend fun deleteDocument(collection: String, document: String): Boolean {
+        return try {
+            Firebase.firestore
+                .collection(collection)
+                .document(document)
+                .delete()
+                .await()
+            true
+        }
+        catch (e: Exception) {
+            Log.d(TAG, "Unable to delete $document from $collection due to $e")
             false
         }
     }
--- app/src/main/java/com/seniorproj/thunderbird/ProfileActivity.kt
+++ app/src/main/java/com/seniorproj/thunderbird/ProfileActivity.kt
@@ -3,6 +3,7 @@ package com.seniorproj.thunderbird
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.ActionBar
 
 
@@ -20,7 +21,7 @@ class ProfileActivity : AppCompatActivity() {
     //FirebaseAuth
     private lateinit var firebaseAuth: FirebaseAuth
 
-    override fun onCreate(savedInstanceState: Bundle?) = runBlocking {
+    override fun onCreate(savedInstanceState: Bundle?) = runBlocking { // = runBlocking needed for suspend funs
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
         binding = ActivityProfileBinding.inflate(layoutInflater)
@@ -33,6 +34,8 @@ class ProfileActivity : AppCompatActivity() {
         //init firebase auth
         firebaseAuth = FirebaseAuth.getInstance()
         checkUser()
+
+        testDatabase()
 
         // handle click, logout
         binding.logoutBtn.setOnClickListener{
@@ -55,5 +58,91 @@ class ProfileActivity : AppCompatActivity() {
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
         }
+    }
+
+    private suspend fun testDatabase() {
+        val tag = "Database Test"
+        // create Cargo
+        val testCargo = Database.Cargo(
+            "test cargo", "test contents", 1.0, 2.0, 3.0, 4.0
+        )
+
+        // create Container
+        val testContainer = Database.Container(
+            "test container", 4.0, 3.0, 2.0, 1.0
+        )
+
+        // add Cargo to database
+        if (Database.setCargo(testCargo)) {
+            Log.d(tag, "Set ${testCargo.name}")
+        }
+
+        // add Container to database
+        if (Database.setContainer(testContainer)) {
+            Log.d(tag, "Set ${testContainer.name}")
+        }
+
+        // get Cargo
+        val gotCargo = Database.getCargo("test cargo")
+        if (gotCargo != null) {
+            Log.d(tag, "Got ${gotCargo.name} with length ${gotCargo.length}")
+        }
+
+        // get Container
+        val gotContainer = Database.getContainer("test container")
+        if (gotContainer != null) {
+            Log.d(tag, "Got ${gotContainer.name} with height ${gotContainer.height}")
+        }
+
+        // get all Cargo
+        val allCargo = Database.getAllCargo()
+        if (allCargo.isNotEmpty()) {
+            Log.d(tag, "All Cargo received with size ${allCargo.size}")
+        }
+
+        // get all Containers
+        val allContainers = Database.getAllContainers()
+        if (allContainers.isNotEmpty()) {
+            Log.d(tag, "All Containers received with size ${allCargo.size}")
+        }
+
+        // get all Cargo names
+        val allCargoNames = Database.getAllCargoNames()
+        if (allCargoNames.isNotEmpty()) {
+            val setCargoNames = allCargo.map { it.name }.toSet()
+            if (setCargoNames == allCargoNames.toSet()) {
+                Log.d(tag, "All Cargo names received: $allCargoNames")
+            }
+            else {
+                Log.d(tag, "Not all Cargo names received")
+            }
+        }
+
+        // get all Container names
+        val allContainerNames = Database.getAllContainerNames()
+        if (allContainerNames.isNotEmpty()) {
+            val setContainerNames = allContainers.map { it.name }.toSet()
+            if (setContainerNames == allContainerNames.toSet()) {
+                Log.d(tag, "All Container names received: $allContainerNames")
+            }
+            else {
+                Log.d(tag, "Not all Container names received")
+            }
+        }
+
+        // delete test Cargo
+        if (Database.deleteCargo(testCargo.name)) {
+            if (Database.getCargo(testCargo.name) == null) {
+                Log.d(tag, "Deleted ${testCargo.name}")
+            }
+        }
+
+        // delete test Container
+        if (Database.deleteContainer(testContainer.name)) {
+            if (Database.getContainer(testContainer.name) == null) {
+                Log.d(tag, "Deleted ${testContainer.name}")
+            }
+        }
+
     }
 }
