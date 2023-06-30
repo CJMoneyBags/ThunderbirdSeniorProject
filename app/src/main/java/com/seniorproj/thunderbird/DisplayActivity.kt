fd68b409bc5c74a99f6d22f2c0a5b5964cf557b6
Chris Bagwell <49886936+CJMoneyBags@users.noreply.github.com>
2023-06-29 19:09:47 -0600

    Drag Dev

--- app/src/main/java/com/seniorproj/thunderbird/DisplayActivity.kt
+++ app/src/main/java/com/seniorproj/thunderbird/DisplayActivity.kt
@@ -2,9 +2,11 @@ package com.seniorproj.thunderbird
 
 import android.content.ClipData
 import android.content.ClipDescription
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.DragEvent
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
@@ -13,6 +15,10 @@ import com.seniorproj.thunderbird.databinding.ActivityDisplayBinding
 class DisplayActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDisplayBinding
+
+    // Counter variable to track the number of copied draggable views
+    private var copiedCount = 0
+    private var maxCopies = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +39,9 @@ class DisplayActivity : AppCompatActivity() {
         binding.section5Bottom.setOnDragListener(dragListener)
         binding.section6Top.setOnDragListener(dragListener)
         binding.section6Bottom.setOnDragListener(dragListener)
+
+
+        maxCopies = 5
 
         //Binding the draggable object(s)
         //TODO: Figure out how to get dynamically populated draggable objects.
@@ -73,29 +82,76 @@ class DisplayActivity : AppCompatActivity() {
             DragEvent.ACTION_DROP -> {
                 val item = event.clipData.getItemAt(0)
                 val dragData = item.text
-                //TODO: Debug and make updates for Toast to print screen.
-                //val Toast.makeText(this, dragData, Toast.LENGTH_SHORT).show()
 
-                val v = event.localState as View
-                val owner = v.parent as ViewGroup
-                owner.removeView(v)
-                val destination = view as LinearLayout
-                destination.addView(v)
-                v.visibility = View.VISIBLE
+                if (view is LinearLayout) {
+//                    val draggableView = createDraggableView()
+//                    view.addView(draggableView)
+//                    draggableView.visibility = View.VISIBLE
+                    if (copiedCount < maxCopies) {
+                        val draggableView = createDraggableView()
+                        view.addView(draggableView)
+                        draggableView.visibility = View.VISIBLE
+
+                        // Increment the counter variable
+                        copiedCount++
+                        // Increment the copiedCount and update the count tex
+                        binding.countText.text = (maxCopies - copiedCount).toString()
+                    }
+                }
+
+                // Revert the visibility of the original view
+                val originalView = event.localState as View
+                originalView.visibility = View.VISIBLE
+
                 true
-
-//                // Create a copy of the draggable image
-//                val draggedImage = event.localState as ImageView
-//                val copyImage = ImageView(this)
-//                copyImage.setImageDrawable(draggedImage.drawable)
             }
+
 
             DragEvent.ACTION_DRAG_ENDED -> {
                 view.invalidate()
+
+                // Check the result of the drag operation
+                if (!event.result) {
+                    // Drag operation was not successful, restore visibility of the draggable box
+                    val draggableBox = event.localState as View
+                    draggableBox.visibility = View.VISIBLE
+                }
+
                 true
             }
 
             else -> false
         }
     }
+
+    private fun createDraggableView(): View {
+        val draggableView = View(this)
+        draggableView.layoutParams = LinearLayout.LayoutParams(
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        draggableView.setBackgroundColor(Color.BLUE) // Set the background color or other desired properties
+        draggableView.setOnTouchListener(touchListener)
+
+        return draggableView
+    }
+    private val touchListener = View.OnTouchListener { view, event ->
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                // Start the drag operation when the view is touched
+                val clipText = "This is our ClipData text"
+                val item = ClipData.Item(clipText)
+                val mimeTypes = arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN)
+                val data = ClipData(clipText, mimeTypes, item)
+
+                val dragShadowBuilder = View.DragShadowBuilder(view)
+                view.startDragAndDrop(data, dragShadowBuilder, view, 0)
+
+                view.visibility = View.INVISIBLE
+                true
+            }
+            else -> false
+        }
+    }
+
 }
